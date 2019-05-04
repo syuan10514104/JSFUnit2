63c2f1ecf654e216c7fcf44fcc892a8f5236c176
syuan <syuan10514104@gmail.com>
2019-05-04 19:25:03 +0800

    first push

---
+++ ShopWeb/src/java/uibeans/page.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uibeans;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author user1
+ */
+@Named(value = "page")
+@Dependent
+public class page {
+
+    /**
+     * Creates a new instance of page
+     */
+    public page() {
+    }
+    
+}
